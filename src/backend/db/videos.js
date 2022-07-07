e8578361096911f0e474bc0c40489b73d3a2adf6
Atharva Bhanage <atharvabhanage2002@gmail.com>
2022-07-07 20:00:48 +0530

    feat : added Videos in database

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -1,21 +1,435 @@
+import { v4 as uuid } from "uuid";
 /**
  * Videos Database can be added here.
  * You can add videos of your wish with different attributes
  * */
-
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "mEOBGY4-7JA",
+    title: "भारत की अविस्मरणीय जीत | 2011 विश्व कप",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "  देखिये कैसे भारत ने श्रीलंका पर शानदार जीत हासिल कर 2011 में अपना दुरसा विश्व कप हासिल किया।",
+    channel: "ICC",
+    thumbnail: "/assets/Thumbnail/video-1.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s48-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/mEOBGY4-7JA?autoplay=1",
+    category: "ODI",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "wY6UyatwVTA",
+    title:
+      "India claim stunning series win, end Australia's Gabba streak | Vodafone Test Series 2020-21",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Rishabh Pant was the hero as an injury-ravaged India ended Australia's 32-year undefeated run at the Gabba, executing a record-breaking final day chase to win the Test by three wickets and the series 2-1.",
+    channel: "cricket.com.au",
+    thumbnail: "/assets/Thumbnail/video-2.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/wY6UyatwVTA?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "s_P8fpLsi",
+    title: "241 runs - with no cover drives! Sachin's SCG epic",
+    description:
+      "Australia legend Steve Waugh looks back on one of the most disciplined Test innings of all time, when Sachin Tendulkar hit 241 in Sydney and refused to play a favourite shot",
+    channel: "cricket.com.au",
+    thumbnail: "/assets/Thumbnail/video-3.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/s_P8fpLsi-A?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "T9SgyrNhTSA",
+    title: "IND v PAK | Bowl-out | 2007 T20WC | Hindi Highlights",
+    description: "",
+    channel: "ICC",
+    thumbnail: "/assets/Thumbnail/video-4.jpg",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s48-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/T9SgyrNhTSA?autoplay=1",
+    category: "T20",
+  },
+  {
+    _id: "yFlarM35vxA",
+    title:
+      "Kohli's FIRST Test Century in England! | Edgbaston 2018 | England Cricket",
+    description:
+      "Watch the moment Kohli broke the curse and secured his first Test century on English soil at Edgbaston in 2018!",
+    channel: "England Cricket",
+    thumbnail: "/assets/Thumbnail/video-5.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQ1IkePr9q5BHgj3WLQcL8Xn1cm7ScXo2NU-Gz7eQ=s48-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/yFlarM35vxA?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "BTG24QY6Vyw",
+    title:
+      "Pujara Dejavu! | History Repeats Itself for Australia in Sydney as Pujara Hits his THIRD Ton",
+    description:
+      "On another slow wicket in the final Test in Sydney Cheteshwar Pujara was at it again, claiming his third hundred of the series as India rack up a huge (& series defining) 622-7",
+    channel: "Amazon Prime",
+    thumbnail: "/assets/Thumbnail/video-6.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLREstWYn-o7E8XoLEomyFHGbjdDO-HwMMLWSLUX=s48-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/BTG24QY6Vyw?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "nOTMWjlamAU",
+    title: "IND v AUS | 2007 T20WC | Hindi Highlights",
+    description:
+      "Australia legend Steve Waugh looks back on one of the most disciplined Test innings of all time, when Sachin Tendulkar hit 241 in Sydney and refused to play a favourite shot",
+    channel: "ICC",
+    thumbnail: "/assets/Thumbnail/video-7.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/nOTMWjlamAU?autoplay=1",
+    category: "T20",
+  },
+  {
+    _id: "ue54scjEjxs",
+    title:
+      "Bhuvi and Sharma's Test Best Figures as India Win at Lord's! | Classic Match | England v India 2014",
+    description:
+      "Rohit's Heroics with Bat and Bumrah's stunning spell shocks ENG at Lords.",
+    channel: "Lord's Cricket Ground",
+    thumbnail: "/assets/Thumbnail/video-8.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/_HpgstMW4b1yFhxT5fdaA-zsOnv4a6qKeF8GT5u_QMEzhmAG9Vq2hYOEM9_LMfEvH8GjrwQQ1Q=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/ue54scjEjxs?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "rpPCjyVfQOo",
+    title:
+      "India hold their nerve to win ODI epic in Canberra | Dettol ODI Series 2020",
+    description:
+      "Watch the full highlights of the action-packed third Dettol ODI in Canberra as India claimed a consolation victory to finish the 50-over series with Hardik Pandya and Shardul Thakur the heroes for the visitors",
+    channel: "cricket.com.au",
+    thumbnail: "/assets/Thumbnail/video-9.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s48-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/rpPCjyVfQOo?autoplay=1",
+    category: "ODI",
+  },
+
+  {
+    _id: "PJutG613i7E",
+    title:
+      "Sharma Stars In Thriller | SUPER OVER REPLAY | BLACKCAPS v India - 3rd T20, 2020",
+    description:
+      "Rohit's Heroics with Bat and Bumrah's stunning spell shocks ENG at Lords.",
+    channel: "England & Wales Cricket Board",
+    thumbnail: "/assets/Thumbnail/video-10.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQbBVJpSNM18auNG6WO8AOJlt6QcAGxb8LZLxLk=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/PJutG613i7E?autoplay=1",
+    category: "T20",
+  },
+  {
+    _id: "M4j4dp-Pj0o",
+    title:
+      "Brave India pull off the great escape at the SCG | Vodafone Test Series 2020-21",
+    description:
+      "A hobbled Hanuma Vihari and a battered Ravichandran Ashwin defy Australia's bowlers for the entire final session after Rishabh Pant and Cheteshwar Pujara laid the platform for a famous drawn Test.",
+    channel: "cricket.com.au",
+    thumbnail: "/assets/Thumbnail/video-11.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/M4j4dp-Pj0o?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "AFEZzf9_EHk",
+    title:
+      "Rohit Sharma Hits 140! | India v Pakistan - Match Highlights | ICC Cricket World Cup 2019",
+    description:
+      "Rohit's Heroics with Bat and Bumrah's stunning spell shocks ENG at Lords.",
+    channel: "England & Wales Cricket Board",
+    thumbnail: "/assets/Thumbnail/video-12.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/AFEZzf9_EHk?autoplay=1",
+    category: "ODI",
+  },
+  {
+    _id: "A-kbmFiCMUI",
+    title: "Kohli, Dhoni too good for the Aussies | Second Gillette ODI",
+    description:
+      " Shaun Marsh hit a brilliant century, Glenn Maxwell found form but India's giants were too good with another Virat Kohli ODI ton and a perfectly-timed finish from MS Dhoni helping India level the series 1-1",
+    channel: "cricket.com.au",
+    thumbnail: "/assets/Thumbnail/video-13.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s48-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/A-kbmFiCMUI?autoplay=1",
+    category: "ODI",
+  },
+  // ***********************************
+  {
+    _id: "vlbIf67PUAM",
+    title: "India Vs South Africa ICC T20 World Cup 2007 ",
+    description: "",
+    channel: "Star Sports",
+    thumbnail: "/assets/Thumbnail/video-14.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSucokengAtD6SUeu2cbP7UynP4_JpwRJ-DX05-=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/vlbIf67PUAM?autoplay=1",
+    category: "T20",
+  },
+  {
+    _id: "yZ3L7wKNDFI",
+    title:
+      "Bumrah Magic In Series Finale | FULL HIGHLIGHTS | BLACKCAPS v India - 5th T20, 2020",
+    description: "",
+    channel: "NZC",
+    thumbnail: "/assets/Thumbnail/video-15.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQbBVJpSNM18auNG6WO8AOJlt6QcAGxb8LZLxLk=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/yZ3L7wKNDFI?autoplay=1",
+    category: "T20",
+  },
+  {
+    _id: "FxdRfTf1pZc",
+    title:
+      "Rohit Breaks Centuries Record In Win | Sri Lanka vs India - Highlights | ICC Cricket World Cup 2019",
+    description: "",
+    channel: "ICC",
+    thumbnail: "/assets/Thumbnail/video-16.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/FxdRfTf1pZc?autoplay=1",
+    category: "ODI",
+  },
+  {
+    _id: "-tOb9blmUXM",
+    title: "Storyboard: Ind vs Aus, 1st Test. Venue: Chennai",
+    description: "",
+    channel: "Star Sports",
+    thumbnail: "/assets/Thumbnail/video-17.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSucokengAtD6SUeu2cbP7UynP4_JpwRJ-DX05-=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/-tOb9blmUXM?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "Mr9o9q3fpno",
+    title: "Smith & Archer Face off at Lords as Australia Aim for Ashes Win!",
+    description: "",
+    channel: "Amazon Prime",
+    thumbnail: "/assets/Thumbnail/video-18.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLREstWYn-o7E8XoLEomyFHGbjdDO-HwMMLWSLUX=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/Mr9o9q3fpno?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "5GTBVklvzIs",
+    title:
+      "When The Crowd Went Wild | Spine-Tingling Test Moments | England Men",
+    description: "",
+    channel: "England Cricket",
+    thumbnail: "/assets/Thumbnail/video-19.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQ1IkePr9q5BHgj3WLQcL8Xn1cm7ScXo2NU-Gz7eQ=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/5GTBVklvzIs?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "CSgioqkF7LQ",
+    title:
+      "Australia vs India 2020-21 | 4th Test Day 5 | Highlights | 19th January, 2021",
+    description: "",
+    channel: "SONY LIV",
+    thumbnail: "/assets/Thumbnail/video-20.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/a4AOsw2qHfuXAooYs7EMkwqJMmW_Wbqebpzm8Xmf2Rj_95QMLnAAFDNRNOJG-sSoairOXvQKrS8=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/CSgioqkF7LQ?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "d5LIyNlq2KY",
+    title:
+      "One Of The Greatest ODI Matches Ever | England v India NatWest Series Final 2002 - Full Highlights",
+    description: "",
+    channel: "England Cricket",
+    thumbnail: "/assets/Thumbnail/video-21.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQ1IkePr9q5BHgj3WLQcL8Xn1cm7ScXo2NU-Gz7eQ=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/d5LIyNlq2KY?autoplay=1",
+    category: "ODI",
+  },
+  {
+    _id: "3sUMaRqoPmI",
+    title:
+      "India all out for 36 I Fifth lowest Test score in history I Fox Cricket",
+    description: "",
+    channel: "FOX Cricket",
+    thumbnail: "/assets/Thumbnail/video-22.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLRPNW4hPeSwuHAKmpqARCZzHu_d--of-8oB18a7=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/3sUMaRqoPmI?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "gkWHGy70jIg",
+    title:
+      "Dhawan Strikes Super Century! | India vs Australia - Match Highlights | ICC Cricket World Cup 2019",
+    description: "",
+    channel: "ICC",
+    thumbnail: "/assets/Thumbnail/video-23.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/gkWHGy70jIg?autoplay=1",
+    category: "ODI",
+  },
+  {
+    _id: "NhbZ76sSQGY",
+    title:
+      "4th Test | India Tour Of England 2021 | Full Match Highlights | Hindi",
+    description: "",
+    channel: "SONY LIV",
+    thumbnail: "/assets/Thumbnail/video-24.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/a4AOsw2qHfuXAooYs7EMkwqJMmW_Wbqebpzm8Xmf2Rj_95QMLnAAFDNRNOJG-sSoairOXvQKrS8=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/NhbZ76sSQGY?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "wamtTEVFDiA",
+    title:
+      "Headingley Final Day HIGHLIGHTS! | Incredible Ben Stokes Wins Match | The Ashes Day 4 2019",
+    description:
+      "What a game, what a day. Relive the final day of the sensational Headingley Test. Is this the best Ashes Test of all time?",
+    channel: "England Cricket",
+    thumbnail: "/assets/Thumbnail/video-25.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQ1IkePr9q5BHgj3WLQcL8Xn1cm7ScXo2NU-Gz7eQ=s48-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/wamtTEVFDiA?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "o6-6WWUfY8U",
+    title:
+      "Captain KOHLI Hits Back For India with a STUNNING Century Against Australia in Perth",
+    description: "",
+    channel: "Amazon Prime",
+    thumbnail: "/assets/Thumbnail/video-26.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLREstWYn-o7E8XoLEomyFHGbjdDO-HwMMLWSLUX=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/o6-6WWUfY8U?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "va3WZ8XbLUA",
+    title: "Exclusive: Re-live India's historic triumph",
+    description:
+      "We have put together a Domain Test series special on India's first-ever Border-Gavaskar series win on Australian soil, a must-watch for Indian fans",
+    channel: "cricket.com.au",
+    thumbnail: "/assets/Thumbnail/video-27.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/va3WZ8XbLUA?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "tHrYQb2n6MQ",
+    title: "India secure series with 27-run win | Second T20I, 2016",
+    description:
+      "Propelled by another Virat Kohli special, India claimed a 27-run victory, while Aaron Finch's fireworks were not enough to save Australia",
+    channel: "cricket.com.au",
+    thumbnail: "/assets/Thumbnail/video-28.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/tHrYQb2n6MQ?autoplay=1",
+    category: "T20",
+  },
+  {
+    _id: "IZz7V9u3jeE",
+    title: "It's Summer in Australia & KING KOHLI is in Town!",
+    description: "",
+    channel: "Amazon Prime",
+    thumbnail: "/assets/Thumbnail/video-29.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLREstWYn-o7E8XoLEomyFHGbjdDO-HwMMLWSLUX=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/IZz7V9u3jeE?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "dMGRTR97Ito",
+    title:
+      "Pandya's power seals series win for India with epic chase | Dettol T20I Series 2020",
+    description: "",
+    channel: "cricket.com.au",
+    thumbnail: "/assets/Thumbnail/video-30.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/dMGRTR97Ito?autoplay=1",
+    category: "T20",
+  },
+  {
+    _id: "-nOdSj0u29U",
+    title:
+      "India Win To Take 2-1 Lead | England v India - Day 5 Highlights | 4th LV= Insurance Test 2021",
+    description: "",
+    channel: "England Cricket",
+    thumbnail: "/assets/Thumbnail/video-31.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQ1IkePr9q5BHgj3WLQcL8Xn1cm7ScXo2NU-Gz7eQ=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/-nOdSj0u29U?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "W2RT3YIPSYU",
+    title:
+      "India level series at MCG with convincing eight-wicket win | Vodafone Test Series 2020-21",
+    description: "",
+    channel: "cricket.com.au",
+    thumbnail: "/assets/Thumbnail/video-32.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/W2RT3YIPSYU?autoplay=1",
+    category: "Tests",
+  },
+  {
+    _id: "amMJfaB5dXo",
+    title:
+      "Rohit Hundred Seals Win | South Africa vs India - Match Highlights | ICC Cricket World Cup 2019",
+    description: "",
+    channel: "ICC",
+    thumbnail: "/assets/Thumbnail/video-33.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/amMJfaB5dXo?autoplay=1",
+    category: "ODI",
+  },
+  {
+    _id: "Mvs1JXYfd6Q",
+    title:
+      "India Win Another Super Over Thriller | FULL HIGHLIGHTS | BLACKCAPS v India - 4th T20, 2020",
+    description:
+      "The visiting Indian team took a 4-0 lead in the five match T20 series against the BLACKCAPS at SKY Stadium in another thriller.",
+    channel: "NZC",
+    thumbnail: "/assets/Thumbnail/video-34.webp",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLQbBVJpSNM18auNG6WO8AOJlt6QcAGxb8LZLxLk=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/Mvs1JXYfd6Q?autoplay=1",
+    category: "T20",
+  },
+  {
+    _id: "PZTo9dlmju8",
+    title:
+      "Muttiah Muralitharan vs Graham Swann Coin hitting Challenge MUST SEE!!",
+    description: "",
+    channel: "Star Sports",
+    thumbnail:
+      "https://i.ytimg.com/an_webp/PZTo9dlmju8/mqdefault_6s.webp?du=3000&sqp=CIXnl5UG&rs=AOn4CLArtIDV-wKxHLUVdc12CMo1Puhwlg",
+    channelIcon:
+      "https://yt3.ggpht.com/ytc/AKedOLSucokengAtD6SUeu2cbP7UynP4_JpwRJ-DX05-=s68-c-k-c0x00ffffff-no-rj",
+    link: "https://www.youtube.com/embed/PZTo9dlmju8?autoplay=1",
+    category: "ODI",
   },
 ];
